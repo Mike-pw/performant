792a3ef4ae2163c3ec1a2597339772e61a184879
Mike Melgren <mikemelgren@gmail.com>
2022-02-01 12:16:08 -0600

    testing netlify forms

--- components/popupWidget.js
+++ components/popupWidget.js
@@ -1,27 +1,17 @@
 import React, { useState } from "react";
-import { useForm, useWatch } from "react-hook-form";
 import { Disclosure, Transition } from "@headlessui/react";
-import { Router } from "next/router";
 
 export default function PopupWidget() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    control,
-    formState: { errors, isSubmitSuccessful, isSubmitting },
-  } = useForm({
-    mode: "onTouched",
-  });
+
   const [isSuccess, setIsSuccess] = useState(false);
 
-  const userName = useWatch({ control, name: "name", defaultValue: "Someone" });
-
-  const onSubmit = (e) => {
+  const handleSubmit = (e) => {
           setIsSuccess(true);
-          reset();
   };
 
+  const reset = (e) => {
+    setIsSuccess(false);
+};
 
   return (
     <div>
@@ -51,7 +41,6 @@ export default function PopupWidget() {
                   <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                 </svg>{" "}
               </Transition>
-
               <Transition
                 show={open}
                 enter="transition duration-200 transform ease"
@@ -81,7 +70,7 @@ export default function PopupWidget() {
               enterFrom="opacity-0 translate-y-5"
               leave="transition duration-200 transform ease"
               leaveTo="opacity-0 translate-y-5">
-              <Disclosure.Panel className="border-none flex flex-col  overflow-hidden left-0 h-full w-full sm:w-[350px] min-h-[250px] sm:h-[600px] border border-gray-300 dark:border-gray-800 bg-white shadow-2xl rounded-md sm:max-h-[calc(100vh-120px)]">
+              <Disclosure.Panel className="flex flex-col  overflow-hidden left-0 h-full w-full sm:w-[350px] min-h-[250px] sm:h-[600px] border border-gray-300 dark:border-gray-800 bg-white shadow-2xl rounded-md sm:max-h-[calc(100vh-120px)]">
                 <div className="flex flex-col items-center justify-center h-32 p-5 bg-red-600">
                   <h3 className="text-lg text-white">How can we help?</h3>
                   <p className="text-white opacity-50">
@@ -89,8 +78,8 @@ export default function PopupWidget() {
                   </p>
                 </div>
                 <div className="flex-grow h-full p-6 overflow-auto bg-gray-50 ">
-                  {!isSubmitSuccessful && (
-                    <form name="contact" netlify method="POST" onSubmit={handleSubmit(onSubmit)} noValidate>
+                  {!isSuccess && (
+                    <form name="contact" netlify method="POST" onSubmit={handleSubmit} noValidate>
 
                       <div className="mb-4">
                         <label
@@ -102,23 +91,8 @@ export default function PopupWidget() {
                           type="text"
                           id="full_name"
                           placeholder="John Doe"
-                          {...register("name", {
-                            required: "Full name is required",
-                            maxLength: 80,
-                          })}
-                          className={`text-gray-800 w-full px-3 py-2 placeholder-gray-300 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring   ${
-                            errors.name
-                              ? "border-red-600 focus:border-red-600 ring-red-100"
-                              : "border-gray-300 focus:border-red-600 ring-red-100"
-                          }`}
-                        />
-                        {errors.name && (
-                          <div className="mt-1 text-sm text-red-400 invalid-feedback">
-                            {errors.name.message}
-                          </div>
-                        )}
+                          className="text-gray-800 w-full px-3 py-2 placeholder-gray-300 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring" />
                       </div>
-
                       <div className="mb-4">
                         <label
                           htmlFor="email"
@@ -128,26 +102,8 @@ export default function PopupWidget() {
                         <input
                           type="email"
                           id="email"
-                          {...register("email", {
-                            required: "Enter your email",
-                            pattern: {
-                              value: /^\S+@\S+$/i,
-                              message: "Please enter a valid email",
-                            },
-                          })}
                           placeholder="you@company.com"
-                          className={`text-gray-800 w-full px-3 py-2 placeholder-gray-300 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring   ${
-                            errors.email
-                              ? "border-red-600 focus:border-red-600 ring-red-100"
-                              : "border-gray-300 focus:border-red-600 ring-red-100"
-                          }`}
-                        />
-
-                        {errors.email && (
-                          <div className="mt-1 text-sm text-red-400 invalid-feedback">
-                            {errors.email.message}
-                          </div>
-                        )}
+                          className="text-gray-800 w-full px-3 py-2 placeholder-gray-300 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring" />
                       </div>
 
                       <div className="mb-4">
@@ -160,47 +116,15 @@ export default function PopupWidget() {
                         <textarea
                           rows="4"
                           id="message"
-                          {...register("message", {
-                            required: "Enter your Message",
-                          })}
                           placeholder="Your Message"
-                          className={`text-gray-800 w-full px-3 py-2 placeholder-gray-300 bg-white border border-gray-300 rounded-md h-28 focus:outline-none focus:ring   ${
-                            errors.message
-                              ? "border-red-600 focus:border-red-600 ring-red-100"
-                              : "border-gray-300 focus:border-red-600 ring-red-100"
-                          }`}
+                          className="text-gray-800 w-full px-3 py-2 placeholder-gray-300 bg-white border border-gray-300 rounded-md h-28 focus:outline-none focus:ring"
                           required></textarea>
-                        {errors.message && (
-                          <div className="mt-1 text-sm text-red-400 invalid-feedback">
-                            {errors.message.message}
-                          </div>
-                        )}
                       </div>
                       <div className="mb-3">
                         <button
                           type="submit"
                           className="w-full px-3 py-4 text-white bg-red-500 rounded-md focus:bg-red-600 focus:outline-none">
-                          {isSubmitting ? (
-                            <svg
-                              className="w-5 h-5 mx-auto text-white animate-spin"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24">
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"></circle>
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
-                          ) : (
-                            "Send Message"
-                          )}
+                          Send Message
                         </button>
                       </div>
                       <p
@@ -210,8 +134,7 @@ export default function PopupWidget() {
                       </p>
                     </form>
                   )}
-
-                  {isSubmitSuccessful && isSuccess && (
+                  {isSuccess && (
                     <>
                       <div className="flex flex-col items-center justify-center h-full text-center text-white rounded-md">
                         <svg
@@ -237,33 +160,6 @@ export default function PopupWidget() {
                         </button>
                       </div>
                     </>
-                  )}
-
-                  {isSubmitSuccessful && !isSuccess && (
-                    <div className="flex flex-col items-center justify-center h-full text-center text-white rounded-md">
-                      <svg
-                        width="60"
-                        height="60"
-                        viewBox="0 0 97 97"
-                        className="text-red-400"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          d="M27.9995 69C43.6205 53.379 52.3786 44.621 67.9995 29M26.8077 29L67.9995 69M48.2189 95C42.0906 95 36.0222 93.7929 30.3604 91.4477C24.6985 89.1025 19.554 85.6651 15.2206 81.3316C10.8872 76.9982 7.44975 71.8538 5.10454 66.1919C2.75932 60.53 1.55225 54.4617 1.55225 48.3333C1.55225 42.205 2.75932 36.1366 5.10454 30.4748C7.44975 24.8129 10.8872 19.6684 15.2206 15.335C19.554 11.0016 24.6985 7.56418 30.3604 5.21896C36.0222 2.87374 42.0906 1.66667 48.2189 1.66667C60.5957 1.66667 72.4655 6.58333 81.2172 15.335C89.9689 24.0867 94.8856 35.9566 94.8856 48.3333C94.8856 60.7101 89.9689 72.58 81.2172 81.3316C72.4655 90.0833 60.5957 95 48.2189 95Z"
-                          stroke="CurrentColor"
-                          strokeWidth="3"
-                        />
-                      </svg>
-
-                      <h3 className="text-xl text-red-400 py-7">
-                        Oops, Something went wrong!
-                      </h3>
-                      <button
-                        className="mt-6 text-red-600 focus:outline-none"
-                        onClick={() => reset()}>
-                        Go back
-                      </button>
-                    </div>
                   )}
                 </div>
               </Disclosure.Panel>
